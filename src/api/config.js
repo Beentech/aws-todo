1cb009127be1bb196784586bc397e6461c6255ca
Ot-A <programming.factory.2022@gmail.com>
2025-01-05 16:59:51 +0900

    getAllの作成

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const ApiUrls = {
+     getTodo:
+         "https://dba1tlwm3a.execute-api.ap-northeast-1.amazonaws.com/default/GetTodo",
+     manageTodo:
+         "https://mnpy486t61.execute-api.ap-northeast-1.amazonaws.com/default/ManageTodo",
+ };
